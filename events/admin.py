ccf42e1217418fcf21af579a4b2430e8c8b428d1
S-Lowes <seblowes@btinternet.com>
2021-08-16 21:43:02 +0000

    (feat): Admin for events has been setup

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Event, Showtime, Venue, Price
+
 
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        'venue',
+        'name',
+        'date_start',
+        'date_end',
+        'run_time',
+        'description_short',
+        'description_long',
+    )
+
+
+class ShowtimeAdmin(admin.ModelAdmin):
+    list_display = (
+        'event',
+        'date',
+        'time',
+    )
+
+
+class Venueadmin(admin.ModelAdmin):
+    list_display = (
+        'showtime',
+        'name',
+        'capacity',
+    )
+
+
+class Priceadmin(admin.ModelAdmin):
+    list_display = (
+        'showtime',
+        'name',
+        'price',
+    )
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Showtime, ShowtimeAdmin)
+admin.site.register(Venue, Venueadmin)
+admin.site.register(Price, Priceadmin)
