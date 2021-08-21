bf8b2aec5ee86981449ab8a43f0006ca7301b8c7
S-Lowes <seblowes@btinternet.com>
2021-08-21 23:40:41 +0000

    (feat): Showtimes.html added and view.py and url.py edited

--- events/views.py
+++ events/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Event
+from django.shortcuts import render, get_object_or_404
+from .models import Event, Showtime
 # Create your views here.
 
 
@@ -13,3 +13,17 @@ def all_events(request):
     }
 
     return render(request, 'events/events.html', context)
+
+
+def event_showtimes(request, event_id):
+    """ A view to show individual event showtimes """
+
+    event = get_object_or_404(Event, pk=event_id)
+    showtimes = Showtime.objects.select_related().filter(event=event_id)
+
+    context = {
+        'showtimes': showtimes,
+        'event': event,
+    }
+
+    return render(request, 'events/showtimes.html', context)
