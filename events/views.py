c4496c11ec83f53961f6a0e7842bdb6dedf2f330
S-Lowes <seblowes@btinternet.com>
2021-08-22 23:22:59 +0000

    (feat): Basic search functionality added with query for name and short/long description

--- events/views.py
+++ events/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Event, Showtime
 # Create your views here.
 
@@ -7,9 +9,20 @@ def all_events(request):
     """ A view to show all events including search query """
 
     events = Event.objects.all()
+    query = None
+
+    if 'search' in request.GET:
+        query = request.GET['search']
+        if not query:
+            messages.error(request, "You didn't enter any search criteria!")
+            return redirect(reverse('events'))
+
+        queries = Q(name__icontains=query) | Q(description_short__icontains=query) | Q(description_long__icontains=query)
+        events = events.filter(queries)
 
     context = {
         'events': events,
+        'search_term': query,
     }
 
     return render(request, 'events/events.html', context)
