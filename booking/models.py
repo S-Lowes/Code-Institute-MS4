0add4d140a0f41e62ebc816188a08e5d9ffad674
S-Lowes <seblowes@btinternet.com>
2021-08-27 22:40:34 +0000

    (feat): Started building model for Booking app

--- booking/models.py
+++ booking/models.py
@@ -1,14 +1,29 @@
+import uuid
+
 from django.db import models
+from profiles.models import UserProfile
+
 
 # Create your models here.
+class Booking(models.Model):
+    booking_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    booking_total_price = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    seat_numbers = models.CharField(max_length=50, null=False, blank=False)
 
-# class Order(models.Model):
+    def _generate_booking_number(self):
+        """
+        Generate a random, unique booking number using UUID
+        """
+        return uuid.uuid4().hex.upper()
 
-#    def __str__(self):
-        #return self.name
+    def __str__(self):
+        return self.booking_number
 
 
-#class Ticket(models.Model):
-
-    #def __str__(self):
-        #return self.name
+# class Ticket(models.Model):
+# def __str__(self):
+# return self.name
