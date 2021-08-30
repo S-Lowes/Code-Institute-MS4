e5ed66ce7535a1127b91b944322ddce130076138
S-Lowes <seblowes@btinternet.com>
2021-08-30 13:51:35 +0000

    (feat): Webhooks testing handler

--- booking/urls.py
+++ booking/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-
+from .webhooks import webhook
 
 urlpatterns = [
     path('<int:showtime_id>/', views.booking, name='booking'),
     path('payment/<int:showtime_id>/', views.payment, name='payment'),
     path('payment_success/<booking_number>/', views.payment_success, name='payment_success'),
+    path('wh/', webhook, name='webhook'),
 ]
