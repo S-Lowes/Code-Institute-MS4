b86230bf8c078ad2fe7e4054d7fa25f0b350af6b
S-Lowes <seblowes@btinternet.com>
2021-08-31 02:29:43 +0000

    (feat): Updated templates for profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from.models import UserProfile
 
 # Create your views here.
-from django.shortcuts import render
 
 
 def profiles(request):
     """ Display the user's profile. """
 
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profiles.html'
     context = {
+        'profile': profile
     }
 
     return render(request, template, context)
